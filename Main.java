79d5bcd74b59b37a6b08e0d13a1a31e2272adf88
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-09-30 07:40:17 +0000

    //Write a method with a random string inoput returns a vowels string as ouput.

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+//Write a method with a random string inoput returns a vowels string as ouput.
+
 class Main {
+  int a = 20,b=-29 ,c=23;
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    StringBuilder sb= new StringBuilder("Good Morning");
+    sb.insert(1, "Friend");
+    System.out.println(sb.toString());
+ System.out.println( "Vowels in input are :"+vowelsOnly("input"));
+
+  }
+  public static String  vowelsOnly(String input){
+  String vowels = "aeiou";
+    StringBuilder   sb  = new StringBuilder();
+    for(char c : input.toCharArray()){
+      if(vowels.contains(String.valueOf(c).toLowerCase())){
+        sb.append(c);
+      }
+    }
+    return sb.toString();
   }
 }
+
+
